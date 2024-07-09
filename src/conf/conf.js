b88fed72062feb98c3966d25fa4f918adbd8d70e
Ashish Mishra <83667639+Ashish-mishra7011@users.noreply.github.com>
2024-07-09 12:17:57 +0530

    Update conf.js

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,6 +3,7 @@ const conf={
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDataBaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    rteId:String(import.meta.env.VITE_RTE_API_KEY)
 }
 export default conf;
